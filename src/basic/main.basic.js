39577782d7f21dd52bdf14857fd90f046fe27698
stoic-park <demige79@gmail.com>
2025-04-15 22:29:50 +0900

    refactor: 상수 분리

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -1,12 +1,4 @@
-// 상수 추출 products
-// TODO: 상수 따로 빼자
-var products = [
-  { id: 'p1', name: '상품1', price: 10000, quantity: 50 },
-  { id: 'p2', name: '상품2', price: 20000, quantity: 30 },
-  { id: 'p3', name: '상품3', price: 30000, quantity: 20 },
-  { id: 'p4', name: '상품4', price: 15000, quantity: 0 },
-  { id: 'p5', name: '상품5', price: 25000, quantity: 10 },
-];
+import { products } from './const';
 
 // 선택 요소, 추가 버튼, 장바구니 표시, 총 금액, 재고 정보
 // TODO: DOM 요소 이름이 애매한데
