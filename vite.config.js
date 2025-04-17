3032e4d1f5c1311364dd3a7fe27215cdd7f7253c
stoic-park <demige79@gmail.com>
2025-04-17 22:55:40 +0900

    chore: 배포 관련 로직 추가

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
 
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/setupTests.js'
-  },
-})
+    base: process.env.NODE_ENV === 'production' ? '/front_5th_chapter2-1/' : '/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                notFound: resolve(__dirname, '404.html'),
+            },
+        },
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: 'src/setupTests.js',
+    },
+});
