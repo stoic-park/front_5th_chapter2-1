d93a955ee4766084172cc8db73de6e53845e3eeb
stoic-park <demige79@gmail.com>
2025-04-18 03:03:35 +0900

    refactor: 즉시실행함수 변경

--- src/basic/logic/calculateCart.js
+++ src/basic/logic/calculateCart.js
@@ -10,6 +10,8 @@ export const calculateCart = (products) => {
     var totalPrice = 0;
     // 총 개수 초기화
     var totalQuantity = 0;
+    // 할인 초기화
+    var discountRate = 0;
 
     // 장바구니 아이템 목록
     const $cartDisplay = document.getElementById('cart-items');
@@ -17,43 +19,39 @@ export const calculateCart = (products) => {
 
     // 장바구니 아이템 목록 반복
     for (var i = 0; i < cartItems.length; i++) {
-        (function () {
-            // 현재 아이템
-            let currentItem;
-            // 상품 목록 반복
-            for (var j = 0; j < products.length; j++) {
-                // 현재 아이템 찾기
-                if (products[j].id === cartItems[i].id) {
-                    currentItem = products[j];
-                    break;
-                }
+        // 현재 아이템
+        let currentItem;
+        // 상품 목록 반복
+        for (var j = 0; j < products.length; j++) {
+            // 현재 아이템 찾기
+            if (products[j].id === cartItems[i].id) {
+                currentItem = products[j];
+                break;
             }
-            console.log(currentItem);
-            // 현재 아이템 개수
-            const quantity = parseInt(cartItems[i].querySelector('span').textContent.split('x ')[1]);
-            // 현재 아이템 총 금액
-            const beforeDiscountPrice = currentItem.price * quantity;
-            // 할인 초기화
-            let discountRate = 0;
-            // 총 개수 증가
-            totalQuantity += quantity;
-            // 총 금액 증가
-            subTotal += beforeDiscountPrice;
-            // 10개 이상 구매 시 할인 적용
-            if (quantity >= 10) {
-                if (currentItem.id === 'p1') discountRate = 0.1;
-                else if (currentItem.id === 'p2') discountRate = 0.15;
-                else if (currentItem.id === 'p3') discountRate = 0.2;
-                else if (currentItem.id === 'p4') discountRate = 0.05;
-                else if (currentItem.id === 'p5') discountRate = 0.25;
-            }
-            // 총 금액 계산
-            totalPrice += beforeDiscountPrice * (1 - discountRate);
-        })();
+        }
+        // 현재 아이템 개수
+        const quantity = parseInt(cartItems[i].querySelector('span').textContent.split('x ')[1]);
+        // 현재 아이템 총 금액
+        const beforeDiscountPrice = currentItem.price * quantity;
+        // 할인 초기화
+        let discountRate = 0;
+        // 총 개수 증가
+        totalQuantity += quantity;
+        // 총 금액 증가
+        subTotal += beforeDiscountPrice;
+
+        // 10개 이상 구매 시 할인 적용
+        if (quantity >= 10) {
+            if (currentItem.id === 'p1') discountRate = 0.1;
+            else if (currentItem.id === 'p2') discountRate = 0.15;
+            else if (currentItem.id === 'p3') discountRate = 0.2;
+            else if (currentItem.id === 'p4') discountRate = 0.05;
+            else if (currentItem.id === 'p5') discountRate = 0.25;
+        }
+        // 총 금액 계산
+        totalPrice += beforeDiscountPrice * (1 - discountRate);
     }
 
-    // 할인 초기화
-    var discountRate = 0;
     // 30개 이상 구매 시 할인 적용
     if (totalQuantity >= 30) {
         // 대량 할인 적용
